1594d52fc8b9d78000bfcc966f31f419db7c5c48
Bastien <bastien.maubon@gmail.com>
2021-02-17 12:12:30 +0100

    m

--- webworker.js
+++ webworker.js
@@ -5,8 +5,9 @@ var intervalle=0;
 function randomCoordinates(){
     x = Math.floor(Math.random()*601);
     y = Math.floor(Math.random()*601);
+    color = "red";
     intervalle = (500 + Math.floor(Math.random()*4500));
-    message  = "{\"x\":" + x + ",\"y\":" + y +"}"
+    message  = "{\"x\":" + x + ",\"y\":" + y + ",\"color\":"+color+"}";
     postMessage(message);
     console.log("Message Posted to worker : " +message)
     setTimeout(function(){randomCoordinates()}, intervalle);
